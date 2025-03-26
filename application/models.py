ecca205ffa0e534a25f430b78e63f78365fdf1c3
GabrielSantosMedeiros <gabriel7.gs61@gmail.com>
2025-03-26 20:11:44 -0300

    create models produto, estoque, consumo

--- application/models.py
+++ application/models.py
@@ -40,4 +40,41 @@ class Reserva(models.Model):
 
     class Meta:
         db_table = 'reservas'
+
+
+
+class Produto(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nome = models.CharField(max_length=128, unique=True)
+    preco_unitario = models.DecimalField(max_digits=10, decimal_places=2)
+    data_registro = models.DateTimeField(default=timezone.now)
+    data_ultima_atualizacao = models.DateTimeField(auto_now=True)
     
+    class Meta:
+        db_table = 'produtos'
+
+
+
+class Estoque(models.Model):
+    id = models.IntegerField(primary_key=True)
+    produto = models.ForeignKey(to=Produto, on_delete=models.CASCADE, null=False, blank=False)
+    quantidade = models.IntegerField(null=False, blank=False)
+    data_registro = models.DateTimeField(default=timezone.now)
+    data_ultima_atualizacao = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'estoque'
+
+
+
+class Consumo(models.Model):
+    id = models.UUIDField(primary_key=True)
+    reserva = models.ForeignKey(to=Reserva, on_delete=models.SET_NULL, null=True, blank=True)
+    produto = models.ForeignKey(to=Produto, on_delete=models.CASCADE, null=False, blank=False)
+    quantidade = models.IntegerField(null=False, blank=False)
+    subtotal = models.DecimalField(max_digits=10, decimal_places=2)
+    data_registro = models.DateTimeField(default=timezone.now)
+    data_ultima_atualizacao = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'consumo'
